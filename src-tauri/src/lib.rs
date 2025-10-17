2b7b7ac4d9ef59d1374d48fcec5e9efd8245355c
RaghavnedraRQ <sai783191@gmail.com>
2025-10-18 00:45:22 +0530

    style(model): Make seperate model mod for each entity \n This is just to test

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 mod request;
 
-
+mod model;
 
 
 #[tauri::command]
--- src-tauri/src/request.rs
+++ src-tauri/src/request.rs
@@ -1,27 +1,7 @@
 
+
 use reqwest::Url;
-
-
-#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
-pub struct Song {
-    pub title: String,
-    pub duration: u16,
-    pub date: std::time::SystemTime,
-    pub liked: bool,
-    pub play_count: u32,
-} 
-
-impl Song {
-    pub fn new(title: String, duration: u16, date: std::time::SystemTime) -> Self {
-        Self {
-            title,
-            duration,
-            date,
-            liked: false,
-            play_count: 0,
-        }
-    }
-}
+use crate::model::song::Song;
 
 #[tauri::command]
 pub async fn sent_request(url: String) -> Result<String, ()> {
