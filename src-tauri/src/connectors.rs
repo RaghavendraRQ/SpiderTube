0ecab72c6bf36f4b108a04097a8f24c7ebc05b51
RaghavnedraRQ <sai783191@gmail.com>
2025-10-23 00:05:34 +0530

    chore(search): Improve search results and thumbnails

--- src-tauri/src/connectors.rs
+++ src-tauri/src/connectors.rs
@@ -1,2 +1,28 @@
+use rustypipe::model::{traits::YtEntity, MusicItem, Thumbnail};
+
 pub mod request;
 pub mod stream;
+
+
+#[derive(Clone, serde::Serialize)]
+pub struct Song {
+    pub id: String,
+    pub name: String,
+    pub thumbnail: Option<Vec<Thumbnail>>
+}
+
+impl Song {
+    pub fn from(music_item: &MusicItem) -> Self {
+        match music_item {
+            MusicItem::Track(track) => {
+                Self {
+                    id: track.id().to_string(),
+                    name: track.name().to_string(),
+                    thumbnail: Some(track.cover.clone())
+                }
+            },
+            _ => Self { id: "fake".to_string(), name: "fake".to_string(), thumbnail: None }
+        }
+        
+    }
+}
