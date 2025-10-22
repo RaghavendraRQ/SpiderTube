fd2bd5db189d6b9c9ad1f0f6db5a41ce96312f35
RaghavnedraRQ <sai783191@gmail.com>
2025-10-22 23:00:50 +0530

    refactor(Test): Testing the search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,41 @@
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import { useState } from "react";
-import ChannelPlayer from "./components/ChannelPlayer";
-import MediaPlayer from "./components/MediaPlayer";
 
 function App() {
-  const [play, setPlay] = useState(false);
-  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
-  const [url, setUrl] = useState("");
-  const [search, setSearch] = useState<string | null>(null);
-  const [result, setResult] = useState<Array<string> | null>(null);
 
+  let [search, setSearch] = useState<string>();
+  let [searchResult, setSearchResult] = useState<string[]>();
 
-  async function songInfo() {
-    // await invoke("get_song_url", {url: url});
-    const url = "dQw4w9WgXcQ";
+  const handleSearch = async function() {
+    if (search == null) {
+      return;
+    }
     try {
-      const report = await invoke<string>("get_song_info", { videoUrl: url });
-      console.log('report', report)
-      // const thumbnail = await invoke("get_track_thumbnail", { videoUrl: url});
-      // console.log('thumbnail', thumbnail)
+      const res = await invoke<Array<string>>("search_result", { trackName: search});
+      setSearchResult(res);
+
     } catch(err) {
       console.log('err', err)
     }
-  }
-
-  async function searchSongs() {
-    if (search == null) {
-      return
-    }
-    try {
-      const res = await invoke<Array<string>>("search_result", {trackName: search});
-      setResult(res);
-    } catch (err) {
-      console.log('err:', err)
-    }
-  }
-
-
-  
-
-  async function PlaySong() {
-    // const filePath = await invoke<string>("fetch_song");
-    // console.log(filePath)
-    if (!audio) {
-      const filePath = await invoke<string>("fetch_song", {url: url})
-      setAudio(new Audio(`data:audio/mpeg;base64,${filePath}`));
-    }
-    setPlay(!play);
-
-    if (play) audio?.pause();
-    else  audio?.play();
-    
-   
- }
+  } 
 
   return (
-    <main className="container">
-      <h1>Spider Tube</h1>
-      <input type="text" placeholder="Search"
-        value={search? search : ""}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <button onClick={searchSongs}> Search Song </button>
-
-
-      {result && 
-        result.map((res) => {
-          return (
-            <li>{res}</li>
-          )
-        })
-      }
-
-     <div>
-        <button onClick={PlaySong}>
-          Play Audio
-        </button>
-
-      </div>
-
-      <div>
-        <button onClick={songInfo}>Click here please</button>
-      </div>
-
-      <ChannelPlayer></ChannelPlayer>
-
-      <div>
-        <MediaPlayer></MediaPlayer>
-      </div>
-
-    </main>
-  );
+    <div>
+      <input type="text"
+      value={search}
+      onChange={(e) => {setSearch(e.target.value)}}
+       />
+       <button onClick={handleSearch}>Search</button>
+       {searchResult && (
+        <ul>
+          {searchResult.map((result, index) => (
+            <li key={index}>{result}</li>
+          ))}
+        </ul>
+       )}
+    </div>
+  )
 }
 
 export default App;
